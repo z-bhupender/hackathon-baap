14d7d4a22e06d3cac05468644671933da08f0e9b
Bhupender <124231740+z-bhupender@users.noreply.github.com>
2025-03-09 11:32:16 +0530

    dev: Updated

--- logic/search/src/routes/searchRouter.js
+++ logic/search/src/routes/searchRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { search, insert } from "../controllers/searchController.js";
+import { search, insert, list } from "../controllers/searchController.js";
 
 const searchRouter = express.Router();
 
 searchRouter.get("/", (req, res) => {
   res.send("Search route");
 });
+
+searchRouter.get("/list", list);
 searchRouter.post("/query", search);
 searchRouter.post("/insert", insert);
 
