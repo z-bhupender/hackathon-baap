0922c6811ba1418fd8f873e3646deeec5511fd4d
Bhupender <124231740+z-bhupender@users.noreply.github.com>
2025-03-09 11:44:09 +0530

    dev: Updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App.jsx";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
