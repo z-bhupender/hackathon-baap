360c8bab0fdf8ecbbd47052e54aa60bf3f01101a
Bhupender <124231740+z-bhupender@users.noreply.github.com>
2025-03-10 01:58:20 +0530

    dev: Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Allow external access
+    port: process.env.PORT || 5173, // Use Render's port or default to 5173
+  },
+});
