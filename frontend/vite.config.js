0609f6ac080041715fcbaa7ecd5212b33f2aa842
Bhupender <124231740+z-bhupender@users.noreply.github.com>
2025-03-10 02:28:21 +0530

    dev: Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    host: "0.0.0.0", // Allow external access
-    port: process.env.PORT || 10000, // Use Render's assigned port
+    host: "0.0.0.0",
+    port: process.env.PORT || 10000,
     strictPort: true,
-    allowedHosts: ["hackathon-baap.onrender.com"], // Your Render domain
+    allowedHosts: ["hackathon-baap.onrender.com"],
   },
   preview: {
     port: process.env.PORT || 10000,
